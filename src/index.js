ea22944ceb4cbb9ca9ad4f9d9cc3f3d3066b977a
lenac0des <ellenajean@gmail.com>
2023-02-26 17:28:50 -0500

    discort bot commit day 1

---
+++ src/index.js
@@ -0,0 +1,66 @@
+/**
+    * @description      : 
+    * @author           : ellen
+    * @group            : 
+    * @created          : 23/02/2023 - 20:58:40
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 23/02/2023
+    * - Author          : ellen
+    * - Modification    : 
+**/
+
+// destructure by creating an object for my bot
+// the reason for destructure is to import things inside 
+// the curly braces into the discord package
+// the client is our bot
+const {Client, IntentsBitField} = require('discord.js');
+
+// create a new client 
+// instents is only required inside the new client object the rest are optional
+// intents will be an array
+// the reason for intents: is a set of permissions that your bot 
+// can use in order to gain access to a set of events
+// refer to discord gateway for intents
+// guild is a server
+// next add more guild intents
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        // to read messages
+        IntentsBitField.Flags.MessageContent,
+    ], 
+});
+
+// c is for client
+client.on('ready', (c) => {
+    // this pulls up the bots usertag
+    console.log(`✨ ${c.user.tag} is aliveee...`);
+});
+
+
+
+
+
+// Create a new event listener that will create the bot's messages
+// this will trigger the bot everytime a user sends out a message
+// the intents above help make this event listner work
+client.on('messageCreate', )
+
+
+// now log into the bot
+// the client is the bot instance
+
+// call the client below
+// add a string inside the paraenthesis which is the token
+client.login('MTA3ODQ4MDk3NDY0MTM3NzM2MQ.GEYueL.BzrzW8CbwqwfBTAOYL5964IW1PVQmAnertxVKg'
+);
+
+// we have to run the file automatically
+// to do so npm install -g nodemon to install it globally
+//  type in nodemon in terminal
+// this will get our bot to go online
+//  add an event listner before the client
