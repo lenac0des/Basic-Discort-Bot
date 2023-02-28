e97bf13cf0077f2ec89b62ae9533257c06098a74
lenac0des <ellenajean@gmail.com>
2023-02-27 19:53:26 -0500

    day 2 content

---
+++ src/register-commands.js
@@ -0,0 +1,13 @@
+/**
+    * @description      : 
+    * @author           : ellen
+    * @group            : 
+    * @created          : 27/02/2023 - 19:52:44
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 27/02/2023
+    * - Author          : ellen
+    * - Modification    : 
+**/
+// a temparay file where slash commands will be made 
